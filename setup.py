56327bac744ee9cda065eeb425e98ed28cf54db7
PR KISHORE KUMAR <134195872+KishoreKumar0295@users.noreply.github.com>
2024-05-12 14:48:57 +0530

    Project final Commit for Git Repo

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
 import setuptools
 
-with open("README.md", "r", encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-__version__ = '0.0.0'
+__version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME= 'Kishore Kumar PR'
-SRC_REPO= "TestSummarizer"
-AUTHOR_EMAIL = 'pr.urkishorekumar@gmail.com'
+AUTHOR_USER_NAME = "entbappy"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
+
+
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description='A small python package for NLP App',
+    description="A small python package for NLP app",
     long_description=long_description,
-    lognd_description_content='text/markdown',
-    url=f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}',
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker" : f'https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues',
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"":"src"},
-    packages=setuptools.find_packages(where="src"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ]
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
